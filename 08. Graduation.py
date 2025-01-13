91a631c1c40173b725e358022ee97145eab1cb8b
Margarita-Simeonova <margito.sl@gmail.com>
2025-01-13 07:54:28 +0200

    08. Graduation
    Graduation

---
+++ 08. Graduation.py
@@ -0,0 +1,23 @@
+def graduation(name, grade):
+    class_counter = 1
+    grade_counter = 0
+
+    while class_counter <= 12:
+
+        if grade < 4:
+            return (f"{name} has been excluded at"
+                    f" {class_counter} grade")
+
+        class_counter += 1
+        grade_counter += grade
+        grade = float(input())
+
+    average_grade = grade_counter / 12
+    return (f"{name} graduated. "
+            f"Average grade: {average_grade}")
+
+
+name_arg = input()
+grade_arg = float(input())
+result = graduation(name_arg, grade_arg)
+print(result)
